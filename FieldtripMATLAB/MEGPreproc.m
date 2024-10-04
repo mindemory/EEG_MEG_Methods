77ea894f30a25aacceb5ec698f20c4f3476ec525
Mrugank Dake <mrugankdake@gmail.com>
2024-10-04 13:49:47 -0400

    working on meg pipeline

--- FieldtripMATLAB/MEGPreproc.m
+++ FieldtripMATLAB/MEGPreproc.m
@@ -1,44 +1,103 @@
 clear; close all; clc;
 
-% Initialize Fieldtrip
-addpath('/Users/mrugankdake/Documents/MATLAB/fieldtrip-20220104');
-ft_defaults;
+% % Define the paths and initialize Fieldtrip
+fieltrip_version = '20220104';
+my_user_id = 'mdd9787';
+curr_dir = pwd;
+path_parts = strsplit(curr_dir, filesep);
+base_dir = fullfile(filesep, path_parts{1:3});
+tool_path = fullfile(base_dir, 'Documents', 'MATLAB');
+dataPath = fullfile(base_dir, 'Library', 'CloudStorage', ...
+    strcat('GoogleDrive-', my_user_id, '@nyu.edu'), ...
+    'My Drive', 'Coursework', 'EEG MEG methods', 'ClassData');
+meg_path = fullfile(dataPath, 'MEG');
+eeg_path = fullfile(dataPath, 'EEG');
 
+addpath(fullfile(tool_path, ['fieldtrip-' fieltrip_version]));
+ft_defaults;
 %% Load data
-% Define the paths
-rootPath = '/Users/mrugankdake/Library/CloudStorage/GoogleDrive-mdd9787@nyu.edu/My Drive/Coursework/EEG MEG methods/ClassData/MEG';
+
 groupName = 'GroupB';
-recRoot = fullfile(rootPath, groupName, 'Recording');
+recRoot = fullfile(meg_path, groupName, 'Recording');
 
 lstFiles = dir(recRoot);
 lstFiles = {lstFiles.name};
 
 % Filter the files based on their extensions
-sqdFiles = lstFiles(endsWith(lstFiles, '_NR.fif'));
+sqdFiles = lstFiles(endsWith(lstFiles, '_NR.sqd'));
+fifFiles = lstFiles(endsWith(lstFiles, '_NR.fif'));
+
 % mrkFiles = lstFiles(endsWith(lstFiles, '.mrk'));
 % elpFiles = lstFiles(endsWith(lstFiles, 'Points.txt'));
-% hsFiles = lstFiles(endsWith(lstFiles, 'HS.txt'));
+hsFiles = lstFiles(endsWith(lstFiles, 'HS.txt'));
 
 % In this case, second file is the one we want to load
-ff = sqdFiles{1};
+ff = sqdFiles{2};
+% or use fif?
+ff = fifFiles{2};
+
+
 % mrk = cellfun(@(x) fullfile(recRoot, x), mrkFiles, 'UniformOutput', false);
 % elp = fullfile(recRoot, elpFiles{1});
 % hsp = fullfile(recRoot, hsFiles{1});
-kitpath = fullfile(recRoot, ff);
+rec_filepath = fullfile(recRoot, ff);
 
 %%
+hdr = ft_read_header(rec_filepath);
 % Load the raw data using FieldTrip
 cfg = [];
-cfg.datafile = kitpath;
-rawData = ft_preprocessing(cfg);
+cfg.datafile = rec_filepath;
+all_rawData = ft_preprocessing(cfg);
+
+%% split the recording channels in data and trigger channels
+cfg = [];
+cfg.channel = all_rawData.grad.label;
+raw_meg_data = ft_selectdata(cfg, all_rawData);
+
+cfg = [];
+cfg.channel = all_rawData.label(...
+    ~ismember(all_rawData.label, all_rawData.grad.label));
+raw_trig_data = ft_selectdata(cfg, all_rawData);
+
+save(fullfile(recRoot, 'all_rawData.mat'), 'all_rawData', '-v7.3');
+
+
+%% plot sensors 
+% ft_plot_sens()
+%% generously cut the data
+% words: 2.5s pre to 5s post (they will overlap)
+% story: 5s pre to 5s post
+
+%% for next week 
+%% overwrite the artifacts (excluding eyes) with nan
+%% compute ICA 
+
 
 data = rawData.trial{1};
 chLabel = rawData.label;
+
+grad = ft_read_sens(rec_filepath);
+cfg = [];
+cfg.grad = grad;
+lay = ft_prepare_layout(cfg);
+
 
 %% Visualize data and screen for artifacts
 megChans = find(contains(chLabel, 'MEG'));
 cfg = [];
 cfg.viewmode = 'vertical';
-cfg.channel = megChans;
 cfg.ylim = [-1e-13, 1e-13];
-ft_databrowser(cfg, rawData)
+cfg.layout = lay;
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+cfg.preproc.hpfiltord = 4;
+
+%% first step mark ALL artifacts
+cfg_out_allArt = ft_databrowser(cfg, raw_meg_data);
+
+%% second step remove artifacts that could be eye related
+
+cfg_out_noEye = ft_databrowser(cfg_out_allArt, rawData);
+
+
+%%
