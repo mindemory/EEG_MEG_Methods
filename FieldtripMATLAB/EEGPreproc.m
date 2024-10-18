9cf767e84745bb4b70bf93c89a71d05898b156b5
Mrugank Dake <mdd9787@log-1.hpc.nyu.edu>
2024-10-18 10:12:53 -0400

    working on eeg preproc

---
+++ FieldtripMATLAB/EEGPreproc.m
@@ -0,0 +1,37 @@
+clear; close all; clc;
+
+% Setting up paths for data
+curr_dir = pwd;
+path_parts = strsplit(curr_dir, filesep);
+fieldtrip_path = fullfile(filesep, path_parts{1:end-1}, 'fieldtrip');
+dataPath = fullfile(filesep, 'scratch', 'work', 'courses', ...
+                'PSYCH-GA-3405-2024fa');
+
+meg_path = fullfile(dataPath, 'MEG');
+eeg_path = fullfile(dataPath, 'EEG');
+
+% Add fieldtrip to path
+addpath(fullfile(fieldtrip_path));
+ft_defaults;
+
+%% TODO: this needs to be BIDS
+groupName = 'GroupD';
+recRoot = fullfile(eeg_path, groupName, 'Recording');
+
+lstFiles = dir(recRoot);
+lstFiles = {lstFiles.name};
+
+% Filter the files based on their extensions
+bdfFiles = lstFiles(endsWith(lstFiles, '.bdf'));
+
+% Load data
+cfg = [];
+cfg.dataset = fullfile(recRoot, bdfFiles{1});
+data = ft_preprocessing(cfg);
+
+% Visualize the data
+cfg = [];
+cfg.viewmode = 'vertical';
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+ft_databrowser(cfg, data)
