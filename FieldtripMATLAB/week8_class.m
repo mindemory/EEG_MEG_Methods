61e76b7f1fd65c815f20347e2937a7c6a2968160
Mrugank Dake <mrugankdake@gmail.com>
2024-10-21 13:34:53 -0400

    python script to convert to bids for EEG ready to roll, week8 class update from Sebastian in progress

---
+++ FieldtripMATLAB/week8_class.m
@@ -0,0 +1,450 @@
+clear; close all; clc;
+
+[ret, hostname] = system('hostname');
+if ret ~= 0
+    hostnme = getenv('HOSTNAME');
+end
+hostname = strtrim(hostname);
+
+if contains(hostname, 'hpc')
+    % You are running your code on HPC
+    curr_dir = pwd;
+    path_parts = strsplit(curr_dir, filesep);
+    fieldtrip_path = fullfile(filesep, path_parts{1:end-1}, 'fieldtrip');
+    dataPath = fullfile(filesep, 'scratch', 'work', 'courses', ...
+                    'PSYCH-GA-3405-2024fa');
+elseif strcmp(hostname, 'sebastian_mac')
+    % You are Sebastian
+    % Setting up paths for data
+    mydir = pwd;
+    path_parts = strsplit(mydir, filesep);
+    fieldtrip_path = fullfile(filesep, path_parts{1:end-1}, 'fieldtrip');
+    idcs   = strfind(mydir, filesep);
+    assert(strcmp(mydir(end-14:end), 'FieldtripMATLAB'), ...
+        'Please cd to folder');
+    dataPath = mydir(1:idcs(end-2)-1);
+else
+    % You are running on your device with Google drive path
+    % Define the paths and initialize Fieldtrip
+    my_user_id = 'mdd9787'; % change this to your netID
+    curr_dir = pwd;
+    path_parts = strsplit(curr_dir, filesep);
+    base_dir = fullfile(filesep, path_parts{1:3});
+    fieldtrip_path = fullfile(base_dir, 'Documents', 'MATLAB', 'fieldtrip-20220104');
+    dataPath = fullfile(base_dir, 'Library', 'CloudStorage', ...
+        strcat('GoogleDrive-', my_user_id, '@nyu.edu'), ...
+        'My Drive', 'Coursework', 'EEG MEG methods', 'ClassData');
+end
+
+eegRoot = fullfile(dataPath, 'EEGBids');
+
+% Add fieldtrip to path
+addpath(fullfile(fieldtrip_path));
+ft_defaults;
+
+%% Load data
+groupName = 'GroupD'; % Change group name to analyze different dataset (valid IDs: GroupA, GroupC, GroupD)
+userName = 'mrugank'; % Make sure to ensure that you are writing to your derivatives 
+taskName = 'oddball';
+% Get subject code for your group
+groupMap = struct('GroupA', '001', 'GroupC', '003', 'GroupD', '004');
+subjCode = groupMap.(groupName);
+
+dataPath = fullfile(eegRoot, ['sub-' subjCode], 'eeg');
+derivPath = fullfile(eegRoot, ['derivatives_' userName], 'preprocessing', ...
+                    ['sub-' subjCode]);
+
+if ~exist(derivPath,'dir'); mkdir(derivPath); end
+saveRoot = ['sub-' subjCode '_task-' taskName '_'];
+
+%% Load data
+cfg = [];
+cfg.dataset = fullfile(dataPath, ...
+                       ['sub-' subjCode '_task-' taskName '_eeg.bdf']);
+cfg.continuous = 'yes';
+data_raw = ft_preprocessing(cfg);
+
+
+%% read in electrodes with known coordinates
+elecPath = fullfile(dataPath, ['sub-' subjCode '_space-CapTrak_electrodes.tsv']);
+elecTbl = readtable(elecPath, 'FileType', 'text');
+elecTbl = rmmissing(elecTbl); % Remove NaN (for EXG channels)
+
+
+%% Keep the electrode positions
+elec = [];
+elec.elecpos = [elecTbl.x, elecTbl.y, elecTbl.z];
+elec.chanpos = elec.elecpos;
+elec.unit = 'mm';
+n_elec = size(elec.elecpos,1);
+n_sample = size(data_raw.trial{1},2);
+SR = data_raw.fsample;
+elec.label = data_raw.label(1:size(elec.elecpos,1));
+data_raw.elec = elec;
+%% prepare the standard layout (we can do this from elec too but need to adjust scale and rotation)
+cfg = [];
+cfg.layout = 'BioSemi256';
+cfg.feedback = 'yes';
+lay = ft_prepare_layout(cfg);
+%%
+% Visualize the data to check for rogue channels
+cfg = [];
+cfg.layout = lay;
+cfg.viewmode = 'vertical';
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+cfg.ylim = [-100 100];
+cfg.blocksize = 10;
+ft_databrowser(cfg, data_raw);
+%% First step is we re-reference the data
+cfg = [];
+cfg.channel = data_raw.label(1:n_elec); % this is the default
+cfg.reref = 'yes';
+cfg.refmethod = 'avg';
+cfg.refchannel = data_raw.label(1:n_elec);
+data_avg = ft_preprocessing(cfg, data_raw);
+
+%%
+cfg = [];
+cfg.layout = lay;
+cfg.viewmode = 'vertical';
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+cfg.ylim = [-10 10];
+cfg.blocksize = 10;
+ft_databrowser(cfg, data_avg);
+%% now identify bad channels
+%'B7, 9, 25?, C18?, 'G30'? !F2 E5
+bad_channels = {'A26', 'B2', 'B3', 'B4', 'B7' 'B8',...
+    'B19', 'C9', 'C30','D15','E5', 'F2' ,'F22',...
+    'G25', 'G29'};
+% go back one step and interpolate
+% then re-reference again!
+% then epoch the data
+
+%% prepare a neighborhood structure for interpolation
+%(we need to know what channels to use)
+cfg = [];
+cfg.method = 'triangulation';
+cfg.feedback = 'yes';
+cfg.elec = elec;
+neighbours = ft_prepare_neighbours(cfg);
+%% Interpolate bad channels
+cfg = [];
+cfg.method = 'weighted';
+cfg.neighbours = neighbours;
+cfg.badchannel = bad_channels; % List your bad channels
+data_interp = ft_channelrepair(cfg, data_raw);
+%% redo the average reference
+cfg = [];
+cfg.channel = data_raw.label(1:n_elec); 
+cfg.reref = 'yes';
+cfg.refmethod = 'avg';
+cfg.refchannel = data_raw.label(1:n_elec);
+data_avg = ft_preprocessing(cfg, data_interp);
+
+% compute the average here
+avg_vec = mean(data_interp.trial{1}(1:n_elec,:));
+% clean up
+clear data_interp;
+%% check the data again to see if we should exclude 
+% more channels
+% if so.. go back to previous step
+cfg = [];
+cfg.layout = lay;
+cfg.viewmode = 'vertical';
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+cfg.ylim = [-10 10];
+cfg.blocksize = 10;
+ft_databrowser(cfg, data_avg);
+%% concatenate with the EOG;
+cfg = [];
+cfg.channel = data_raw.label(n_elec+1:n_elec+4);
+
+data_eye = ft_selectdata(cfg, data_raw);
+
+% because eog channels are recorded with amplifier refrence, we need to
+% re-reference them as well
+data_eye.trial{1} = data_eye.trial{1} - avg_vec;
+% EOG channels are recognized by databrowser via ft_channelselection
+data_eye.label = {'EOG-1'; 'EOG-2'; 'EOG-3';'EOG-4'};
+data_combined = ft_appenddata([],data_avg, data_eye);
+
+
+%% now manually mark artifacts
+cfg = [];
+cfg.layout = lay;
+cfg.viewmode = 'vertical';
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+cfg.ylim = [-10 10];
+cfg.blocksize = 10;
+cfg.eogscale = 0.8; % Four channels are eye channels
+cfg_art = ft_databrowser(cfg, data_combined);
+%% compute one big ICA ...
+artifact_def = cfg_art.artfctdef.visual.artifact;
+art_vec = zeros(1, n_sample);
+for aa = 1 : size(artifact_def,1)
+    art_vec(artifact_def(aa,1):...
+        artifact_def(aa,2)) = 1;
+end
+% get the rank of the data (is 240 here because we 
+% excluded -15 channels and did avg reference -1)
+
+%% ... without eye channels
+
+rnk = rank(data_avg.trial{1});
+
+% highpass filter for ica
+cfg = [];
+cfg.hpfilter = 'yes';
+cfg.hpfreq = 1;
+data_nan = ft_preprocessing(cfg, data_avg);
+
+% mask artifacts for ICA
+data_nan.trial{1}(:,art_vec==1)  = nan;
+
+% compute the ICA on masked+filtered data
+cfg = [];
+cfg.method = 'runica';
+cfg.numcomponent= rnk;
+data_comp = ft_componentanalysis(cfg, data_nan);
+
+cfg = [];
+cfg.layout = lay;
+cfg.viewmode = 'component';
+ft_databrowser(cfg, data_comp);
+% remove 1 5? 7
+
+%% ... or with eye channels
+rnk = rank(data_combined.trial{1});
+
+% highpass filter for ica
+cfg = [];
+cfg.hpfilter = 'yes';
+cfg.hpfreq = 1;
+data_nan = ft_preprocessing(cfg, data_combined);
+
+% mask artifacts for ICA
+data_nan.trial{1}(:,art_vec==1)  = nan;
+
+% compute the ICA on masked+filtered data
+cfg = [];
+cfg.method = 'runica';
+cfg.numcomponent= rnk;
+data_comp = ft_componentanalysis(cfg, data_nan);
+%% add eye channels to layout
+
+% add eye positions to layout
+layeye = lay;
+layeye.label = [lay.label; {'EOG-1'; 'EOG-2'; 'EOG-3'; 'EOG-4'}];
+% layeye.pos = [lay.pos; [-0.4 0.28]; [-0.2  0.4]; [0.2 0.4]; [0.4 0.28]]; % Example positions
+layeye.pos = [lay.pos; [-0.49 0.49]; [-0.48 0.48];  [0.48 0.48]; [0.49 0.49]]; % Example positions
+
+layeye.width = [lay.width;ones(4, 1).*lay.width(1)];
+layeye.height = [lay.width;ones(4, 1).*lay.width(1)];
+cfg = []; cfg.layout = layeye;
+ft_layoutplot(cfg)
+%%
+cfg = [];
+cfg.layout = layeye;
+% cfg.preproc.hpfilter = 'yes';
+% cfg.preproc.hpfreq = 0.5;
+
+cfg.viewmode = 'component';
+ft_databrowser(cfg, data_comp);
+
+% reject 1, 3(pulse),  10?hz-eye and 13
+%% use unmixing matrix on unfiltered data
+cfg = [];
+cfg.unmixing = unmixing_eye;
+cfg.topolabel = data_combined.label;
+data_comp = ft_componentanalysis(cfg, data_combined);
+%% reject components:
+cfg           = [];
+cfg.component = [1 3 10, 13];
+data_clean    = ft_rejectcomponent(cfg,data_comp, ...
+    data_combined);
+%% 
+cfg = [];
+cfg.layout = layeye;
+cfg.viewmode = 'vertical';
+cfg.viewmode = 'vertical';
+cfg.preproc.hpfilter = 'yes';
+cfg.preproc.hpfreq = 0.5;
+cfg.ylim = [-10 10];
+ft_databrowser(cfg, data_clean)
+
+save(fullfile(derivPath, [saveRoot 'data_clean.mat']), 'data_clean', '-v7.3');
+clear data_comp data_combined data_raw data_avg
+%%
+%% read in trigger info
+event_info = ...
+    getEvents(...
+    fullfile(dataPath,...
+    ['sub-' subjCode '_task-oddball_events.tsv']));
+n_events = size(event_info,1);
+
+sum(event_info.trialNumber ~=0)
+n_trials = sum(event_info.trialNumber ~=0 & ...
+    event_info.trigger_codes ~=16 ) ;
+samp = nan(n_trials, 1);
+info = cell(n_trials,1);
+inf.group = '';
+inf.type = '';
+trial_idx = 0;
+
+for ee = 1: n_events
+    switch event_info.trigger_codes_fixed(ee)
+        case 1
+            inf.group = 'tone';
+        case 2
+            inf.group = 'vis';
+        case 4
+            inf.group = 'aud';
+        case 8
+            inf.group = 'story_start';
+        case 16
+            inf.group = 'story_seg';
+        case 64
+            trial_idx = trial_idx+1;
+            samp(trial_idx) =  ...
+                event_info.sampleOnset(ee);
+
+            info{trial_idx} = inf;
+            info{trial_idx}.blockType = ...
+                event_info.blockType(ee);
+            info{trial_idx}.trialnumber = ...
+                event_info.trialNumber(ee);
+            info{trial_idx}.sampleinfo = ...
+                event_info.sampleOnset(ee);
+            info{trial_idx}.type  = 'even';
+            info{trial_idx}.blocknumber = ...
+                event_info.blockNumber(ee);
+            info{trial_idx}.trigger =  ...
+               event_info.trigger_codes_fixed(ee);
+        case 128
+            trial_idx = trial_idx+1;
+            samp(trial_idx) =  ...
+                event_info.sampleOnset(ee);
+            info{trial_idx} = inf;
+            info{trial_idx}.blockType = ...
+                event_info.blockType(ee);
+            info{trial_idx}.trialnumber = ...
+                event_info.trialNumber(ee);
+            info{trial_idx}.sampleinfo = ...
+                event_info.sampleOnset(ee);
+            info{trial_idx}.type  = 'odd';
+            info{trial_idx}.blocknumber = ...
+                event_info.blockNumber(ee);
+            info{trial_idx}.trigger =  ...
+                event_info.trigger_codes_fixed(ee);
+            
+    end
+
+end
+% NOTE: blocktype is correct
+% legth of story is 742s 
+
+%% overwrite trialinfo:
+
+%%
+
+% ?? maybe filter?
+% cfg = [];
+% cfg.hpfilter = 'yes';
+% cfg.hpfreq = 0.5;
+% data_clean = ft_preprocessing(cfg, data_clean);
+% 
+% cut into 3 second trials (lots of ovelap)
+cfg = [];
+cfg.trl = [samp-1*SR, samp+2*SR, ...
+    repmat(-1*SR, [n_trials,1])];
+data_cut = ft_redefinetrial(cfg, data_clean);
+clear data_clean;
+%%
+% also remove eye channels
+cfg = [];
+cfg.channel = data_cut.label(1:n_elec);
+data_cut = ft_selectdata(cfg, data_cut);
+%% inspect one more time
+cfg = [];
+cfg.preproc.detrend = 'yes'; % now we detrend instead of highpass filtering! 
+cfg.viewmode = 'vertical';
+cfg.ylim = [-10 10];
+ft_databrowser(cfg, data_cut);
+%% overwrite trialingo
+data_cut.trialinfo = info;
+
+%% now we do "rough" preprocessing
+
+% instead, we should define artifacts manually and check for overlap with
+% the sampleinfo!
+
+cfg = [];
+cfg.preproc.detrend = 'yes'; % now we detrend instead of highpass filtering! 
+cfg.method = 'summary';
+data_pruned = ft_rejectvisual(cfg, data_cut);
+
+
+% automatic muscle artifact detection
+cfg = [];
+cfg.metric = 'zvalue'
+cfg.preproc.bpfilter    = 'yes';
+cfg.preproc.bpfreq      = [70 90];% [110 140] this is EU setting
+cfg.preproc.bpfiltord   =  8;
+cfg.preproc.bpfilttype  = 'but';
+cfg.preproc.rectify     = 'yes';
+cfg.preproc.boxcar      = 0.2;
+data_pruned = ft_rejectvisual(cfg, data_pruned);
+
+%% processing for ERP
+
+% TODO select only 
+cfg = [];
+cfg.detrend = 'no';
+cfg.lpfilter = 'yes';
+cfg.lpfreq = 15;
+cfg.detrend = 'yes';
+cfg.demean              = 'yes';    % we demean (baseline correct) ...
+cfg.baselinewindow      = [-0.5 0]; % using the mean activity in this window
+%      = ft_preprocessing(cfg, data_pruned);
+%%
+cfg = [];
+cfg.trials = find(...
+    cellfun(@(x) ...
+    strcmp('semanticVis', string(x.blockType)), ...
+    data_bl.trialinfo));
+
+tl_vis = ft_timelockanalysis(cfg, data_bl);
+cfg.trials = find(...
+    cellfun(@(x) ...
+    strcmp('semanticAud', string(x.blockType)), ...
+    data_bl.trialinfo));
+tl_aud = ft_timelockanalysis(cfg, data_bl);
+
+
+% 
+% cfg.keeptrials = 'yes';
+% tl_kt = ft_timelockanalysis(cfg, data_bl);;
+
+avg_new = trimmean(tl_kt.trial, 20);
+
+tl.avg      = squeeze(avg_new);
+% keep as template
+
+% do manual trimmean and overwrite
+
+% compute lda between conditions
+
+
+figure;
+cfg =[];
+cfg.layout = lay;
+ft_multiplotER(cfg, tl_vis, tl_aud);
+
+
+cfg =[];
+cfg.layout = lay;
+ft_multiplotER(cfg, tl_odd, tl_even);
---
+++ MNEPython/eeg2bids.py
@@ -0,0 +1,320 @@
+import numpy as np
+import os, mne
+from datetime import datetime
+from mne_bids import write_raw_bids, BIDSPath
+from mne.channels import make_dig_montage
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+# Load the data
+rootPath = '/Users/mrugankdake/Library/CloudStorage/GoogleDrive-mdd9787@nyu.edu/My Drive/Coursework/EEG MEG methods/ClassData/EEG'
+bids_root = '/Users/mrugankdake/Library/CloudStorage/GoogleDrive-mdd9787@nyu.edu/My Drive/Coursework/EEG MEG methods/ClassData/EEGBids'
+groupName = 'GroupD'
+recRoot = os.path.join(rootPath, groupName, 'Recording')
+timeStampsRoot = os.path.join(rootPath, groupName, 'TimeStamps')
+
+# Load electrode coordinates
+elecTsv = os.path.join(recRoot, 'electrodeCoords.tsv')
+electrodes_df = pd.read_csv(elecTsv, sep='\t')
+chNames = electrodes_df['name'].tolist()
+chCoords = electrodes_df[['x', 'y', 'z']].values
+chPos = dict(zip(chNames, chCoords))
+
+# Load fiducials and headshape from a .pos file
+posFile = os.path.join(recRoot, 'fiducials_and_headshape.tsv')
+fiducials_and_headshape = pd.read_csv(posFile, sep='\t', header=None, names=['Type', 'X', 'Y', 'Z'],  keep_default_na=False)
+
+fiducials = {}
+for _, row in fiducials_and_headshape.iterrows():
+    if row['Type'] in ['LPA', 'RPA', 'NA']:
+        fiducials[row['Type']] = [row['X'], row['Y'], row['Z']]
+headshape_points = fiducials_and_headshape[fiducials_and_headshape['Type'] == 'EXTRA'][['X', 'Y', 'Z']].astype(float).values
+
+dig_ch_pos = {
+    'LPA': fiducials['LPA'], 
+    'RPA': fiducials['RPA'],
+    'NAS': fiducials['NA'],
+}
+
+montage = make_dig_montage(ch_pos=chPos,
+                            coord_frame='head',
+                            nasion=fiducials['NA'],
+                            lpa=fiducials['LPA'],
+                            rpa=fiducials['RPA'],
+                            hsp=headshape_points)
+# Visualize the montage
+# montage.plot(kind='3d')
+# plt.show()
+
+# Get the list of all files in recRoot
+recFiles = os.listdir(recRoot)
+
+bdfFiles = [f for f in recFiles if f.endswith('.bdf')]
+raw = mne.io.read_raw_bdf(os.path.join(recRoot, bdfFiles[0]), preload=False)
+raw.info['line_freq'] = 60 # Set the line frequency to 60 Hz
+
+# Remove the last 4 EXG channels
+chansToRemove = ['EXG5', 'EXG6', 'EXG7', 'EXG8', 'GSR1', 'GSR2', 'Erg1', 'Erg2', 'Resp', 'Plet', 'Temp']
+raw.drop_channels(chansToRemove)
+non_eeg_channels = {
+    'EXG1': 'eog',
+    'EXG2': 'eog',
+    'EXG3': 'eog',
+    'EXG4': 'eog',
+}
+raw.set_channel_types(non_eeg_channels)
+raw.set_montage(montage, on_missing='ignore')
+
+
+def fix_missing_flags(events_df):
+    ''' 
+    ome of the runs have missing flags for the start of the block. This code fixes the missing flags 
+    by inserting a new row before the missing flag and updating the blockNumber and trialNumber accordingly.
+    It also fixes the trialNumber for the next 30 trials after the missing flag.
+    '''
+    blkIdx = 1
+    while True:
+        max_block_number = events_df['blockNumber'].max()
+        if blkIdx >= max_block_number:
+            break
+        trlCount = events_df[events_df['blockNumber'] == blkIdx]['trialNumber'].max()
+        blkTypeThis = events_df[events_df['blockNumber'] == blkIdx]['blockType'].values[0]
+
+        if blkTypeThis != 'storyStart':
+            if trlCount > 30:
+                # Find index of trial 31 in the block
+                trl31Idx = events_df[(events_df['blockNumber'] == blkIdx) & (events_df['trialNumber'] == 31)].index[0]
+                # Find index of first trial after where it is either 0 or 1
+                trlNextIdx = events_df[(events_df['blockNumber'] == blkIdx+1) & ((events_df['trialNumber'] == 0) | (events_df['trialNumber'] == 1))].index[0]
+                
+                # Add blocknumber+1 for all blocks starting at trl31Idx
+                events_df.loc[trl31Idx:, 'blockNumber'] += 1
+                # Change trialNumber by subtracting 30 from trl31Idx to next 30 trials
+                events_df.loc[trl31Idx:trlNextIdx-1, 'trialNumber'] -= 30
+
+                # Insert a new row before trl31Idx
+                sample_onset_avg = int((events_df.loc[trl31Idx, 'sampleOnset'] + events_df.loc[trl31Idx-1, 'sampleOnset']) / 2)
+                time_onset_avg = (events_df.timeOnset[trl31Idx] + events_df.timeOnset[trl31Idx-1]) / 2
+                last_trigger_code = events_df[(events_df['blockNumber'] <= blkIdx) & (events_df['trialNumber'] == 0)]['trigger_codes'].values[-1]
+                
+                # Get trialOverBlocks from previous row and blockType from trlIdx row
+                trial_over_blocks_prev = events_df.loc[trl31Idx-1, 'trialoverBlocks']
+                # block_type_current = events_df.loc[trl31Idx, 'blockType']
+                block_type_current = 'Unsure'
+
+                new_row = {
+                    'sampleOnset': sample_onset_avg,
+                    'timeOnset': time_onset_avg,
+                    'offset': 0.0,
+                    'trigger_codes': last_trigger_code,
+                    'blockNumber': blkIdx + 1,
+                    'trialNumber': 0,
+                    'trialoverBlocks': trial_over_blocks_prev,
+                    'trlType': None,
+                    'blockType': block_type_current
+                }
+                events_df = pd.concat([events_df.iloc[:trl31Idx], pd.DataFrame([new_row]), events_df.iloc[trl31Idx:]]).reset_index(drop=True)
+        else:
+            if trlCount > 8:
+                # Find index of trial 9 in the block
+                trl9Idx = events_df[(events_df['blockNumber'] == blkIdx) & (events_df['trialNumber'] == 9)].index[0]
+                # Find index of first trial after where it is either 0 or 1
+                trlNextIdx = events_df[(events_df['blockNumber'] == blkIdx+1) & ((events_df['trialNumber'] == 0) | (events_df['trialNumber'] == 1))].index[0]
+                
+                # Add blocknumber+1 for all blocks starting at trl9Idx
+                events_df.loc[trl9Idx:, 'blockNumber'] += 1
+                # Change trialNumber by subtracting 8 from trl9Idx to next 8 trials
+                events_df.loc[trl9Idx:trlNextIdx-1, 'trialNumber'] -= 8
+
+                # Insert a new row before trl9Idx
+                sample_onset_avg = int((events_df.loc[trl9Idx, 'sampleOnset'] + events_df.loc[trl9Idx-1, 'sampleOnset']) / 2)
+                time_onset_avg = (events_df.timeOnset[trl9Idx] + events_df.timeOnset[trl9Idx-1]) / 2
+                last_trigger_code = events_df[(events_df['blockNumber'] <= blkIdx) & (events_df['trialNumber'] == 0)]['trigger_codes'].values[-1]
+                
+                # Get trialOverBlocks from previous row and blockType from trlIdx row
+                trial_over_blocks_prev = events_df.loc[trl9Idx-1, 'trialoverBlocks']
+                # block_type_current = events_df.loc[trl9Idx, 'blockType']
+                block_type_current = 'Unsure'
+
+                new_row = {
+                    'sampleOnset': sample_onset_avg,
+                    'timeOnset': time_onset_avg,
+                    'offset': 0.0,
+                    'trigger_codes': last_trigger_code,
+                    'blockNumber': blkIdx + 1,
+                    'trialNumber': 0,
+                    'trialoverBlocks': trial_over_blocks_prev,
+                    'trlType': None,
+                    'blockType': block_type_current
+                }
+                events_df = pd.concat([events_df.iloc[:trl9Idx], pd.DataFrame([new_row]), events_df.iloc[trl9Idx:]])
+
+        blkIdx += 1
+    return events_df
+
+def addStimulusMeta(events_df, timeStampsRoot):
+    timeStampsFiles = os.listdir(timeStampsRoot)
+    timeStampsFiles = [f for f in timeStampsFiles if f.endswith('.mat')]
+    timeStampsFiles.sort()  # Sort files in ascending order of time
+    categories = [filename.split('_')[-1].split('.')[0] for filename in timeStampsFiles]
+    blkTypeActual = [filename.split('_')[-2].split('_')[0] for filename in timeStampsFiles]
+    events_df['stimulusCategory'] = None
+    events_df['stimulusName'] = None
+    events_df['stimulusDuration'] = None
+    events_df['blockTypeActual'] = None
+    events_df['trigger_codes_fixed'] = events_df['trigger_codes']
+
+    maxBlocks = events_df['blockNumber'].max()
+    for i, blkIdx in enumerate(range(1, maxBlocks+1)):
+        print(f'Processing block {blkIdx}')
+        trlIdx = events_df[events_df['blockNumber'] == blkIdx].index
+        events_df.loc[trlIdx, 'stimulusCategory'] = categories[i]
+        if blkTypeActual[i] == 'saud':
+            events_df.loc[trlIdx, 'blockTypeActual'] = 'semanticAud'
+            events_df.loc[trlIdx[0], 'trigger_codes_fixed'] = 4
+        elif blkTypeActual[i] == 'svis':
+            events_df.loc[trlIdx, 'blockTypeActual'] = 'semanticVis'
+            events_df.loc[trlIdx[0], 'trigger_codes_fixed'] = 2
+        elif blkTypeActual[i] == 'caud':
+            events_df.loc[trlIdx, 'blockTypeActual'] = 'classicalAud'
+            events_df.loc[trlIdx[0], 'trigger_codes_fixed'] = 1
+        elif blkTypeActual[i] == 'timingData':
+            events_df.loc[trlIdx, 'blockTypeActual'] = 'story'
+            events_df.loc[trlIdx[0], 'trigger_codes_fixed'] = 8
+
+        # Load the timeStamps file
+        timeStampsFile = os.path.join(timeStampsRoot, timeStampsFiles[i])
+        timeStamps = loadmat(timeStampsFile)
+        if categories[i] != 'story2':
+            timingData = timeStamps['timingData']
+
+            # Get stimulus names
+            sti_names = [None] + [entry['stiName'][0] for entry in timingData[0]]
+            events_df.loc[trlIdx, 'stimulusName'] = sti_names
+
+            # Get stimulus duration
+            sti_durations = [None] + [round(entry['offsetTime'][0][0] - entry['onsetTime'][0][0], 4) for entry in timingData[0]]
+            events_df.loc[trlIdx, 'stimulusDuration'] = sti_durations
+
+        else:
+            timingData = timeStamps['timingData3']
+
+            # Get event names
+            sti_names = [None] + [entry['event'][0] for entry in timingData[0]]
+            events_df.loc[trlIdx, 'stimulusName'] = sti_names
+
+            # Get event durations
+            sti_durations = [None] + [round(entry['offsetTime'][0][0] - entry['onsetTime'][0][0], 4) for entry in timingData[0]]
+            events_df.loc[trlIdx, 'stimulusDuration'] = sti_durations
+
+    return events_df
+
+
+# Extract events from STATUS channel
+status_channel = 'Status'
+if status_channel in raw.ch_names:
+    status_data = raw.get_data(picks=[status_channel])[0]
+    times = raw.times
+    
+    event_samples = []
+    event_times = []
+    event_values = []
+    trl_types = []
+    block_types = []
+    block_numbers = []
+    trial_numbers_within_block = []
+    trial_numbers_over_blocks = []
+
+    current_block_type = None
+    current_block_number = 0
+    current_trial_number = 0
+    global_trial_number = 0
+
+    for i in range(1, len(status_data)):
+        if status_data[i] != status_data[i-1]:
+            event_samples.append(i)
+            event_times.append(times[i])
+            event_values.append(status_data[i])
+
+            if status_data[i] == 64:
+                trl_types.append('even')
+            elif status_data[i] == 128:
+                trl_types.append('odd')
+            elif status_data[i] == 16:
+                trl_types.append('storySeg')
+            else:
+                trl_types.append(None)
+
+            # Determine block type based on trigger code
+            if status_data[i] in [1, 2, 4, 8]:
+                if status_data[i] == 1:
+                    current_block_type = 'classicalAud'
+                elif status_data[i] == 2:
+                    current_block_type = 'semanticVis'
+                elif status_data[i] == 4:
+                    current_block_type = 'semanticAud'
+                elif status_data[i] == 8:
+                    current_block_type = 'storyStart'
+
+                current_block_number += 1
+                current_trial_number = 0
+
+            elif status_data[i] in [16, 64, 128]:
+                current_trial_number += 1
+                global_trial_number += 1
+
+            # Append the current block type to the block_types list
+            block_types.append(current_block_type)
+            block_numbers.append(current_block_number)
+            trial_numbers_within_block.append(current_trial_number)
+            trial_numbers_over_blocks.append(global_trial_number)
+            # checker.append(0)
+
+    events_df = pd.DataFrame({
+        'sampleOnset': event_samples,
+        'timeOnset': event_times,
+        'offset': [0.0] * len(event_times),
+        'trigger_codes': event_values,
+        'blockNumber': block_numbers,
+        'trialNumber': trial_numbers_within_block,
+        'trialoverBlocks': trial_numbers_over_blocks,
+        'trlType': trl_types,
+        'blockType': block_types,
+    })
+
+    # Set column trigger_codes to be of type int
+    events_df['trigger_codes'] = events_df['trigger_codes'].astype(int)
+    valid_trigger_codes = [1, 2, 4, 8, 16, 64, 128]
+    events_df = events_df[events_df['trigger_codes'].isin(valid_trigger_codes)]
+
+    # Reset index
+    events_df.reset_index(drop=True, inplace=True)
+
+    # Fix missing flags
+    events_df = fix_missing_flags(events_df)
+    print(events_df['blockNumber'].max()) 
+
+    # Add stimulus meta data
+    events_df = addStimulusMeta(events_df, timeStampsRoot)
+    
+
+# Set the subject ID and task name
+if groupName[-1] == 'A':
+    subject_id = '001'
+elif groupName[-1] == 'B':
+    subject_id = '002'
+elif groupName[-1] == 'C':
+    subject_id = '003'
+elif groupName[-1] == 'D':
+    subject_id = '004'
+
+taskName = 'oddball'
+ 
+# Write the raw data to BIDS
+bids_path = BIDSPath(subject=subject_id, task=taskName, root=bids_root)
+write_raw_bids(raw=raw, bids_path=bids_path, overwrite=True, verbose=True)
+# Write the events to a TSV file
+events_tsv_path = os.path.join(bids_root, 'sub-' + subject_id, 'eeg', 'sub-' + subject_id + '_task-' + taskName + '_events.tsv')
+os.makedirs(os.path.dirname(events_tsv_path), exist_ok=True)
+events_df.to_csv(events_tsv_path, sep='\t', index=False)
